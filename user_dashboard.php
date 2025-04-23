e7a61eebda136d664445757b5be230d8c75cca9f
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-23 17:43:57 +0200

    added kWh usage card for the user

--- user_dashboard.php
+++ user_dashboard.php
@@ -101,6 +101,17 @@ if (isset($_SESSION['customer_id']) && !empty($_SESSION['customer_id'])) {
 	<main class="content-wrap">
 		<div class="content">
 			<section class="info-boxes">
+			<div class="info-box">
+					<div class="box-icon">
+						<svg fill="#99a0b0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" d="M16.168 2.924L4.51 13.061a.25.25 0 00.164.439h5.45a.75.75 0 01.692 1.041l-2.559 6.066 11.215-9.668a.25.25 0 00-.164-.439H14a.75.75 0 01-.687-1.05l2.855-6.526zm-.452-1.595a1.341 1.341 0 012.109 1.55L15.147 9h4.161c1.623 0 2.372 2.016 1.143 3.075L8.102 22.721a1.149 1.149 0 01-1.81-1.317L8.996 15H4.674c-1.619 0-2.37-2.008-1.148-3.07l12.19-10.6z"></path></g></svg>
+					</div>
+					
+					<div class="box-content">
+						<span class="big"><?php foreach ($billing as $bill) { echo $bill['kWh_usage']; } ?></span>
+						Electricity usage (kWh)
+					</div>
+				</div>
+
 				<div class="info-box">
 					<div class="box-icon">
 					<svg fill="#99a0b0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" d="M16.168 2.924L4.51 13.061a.25.25 0 00.164.439h5.45a.75.75 0 01.692 1.041l-2.559 6.066 11.215-9.668a.25.25 0 00-.164-.439H14a.75.75 0 01-.687-1.05l2.855-6.526zm-.452-1.595a1.341 1.341 0 012.109 1.55L15.147 9h4.161c1.623 0 2.372 2.016 1.143 3.075L8.102 22.721a1.149 1.149 0 01-1.81-1.317L8.996 15H4.674c-1.619 0-2.37-2.008-1.148-3.07l12.19-10.6z"></path></g></svg>
